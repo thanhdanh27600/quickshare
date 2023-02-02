b02291317fd451fa552638f6dd2f6b988e4e454e
thanhdanh27600 <thanhdanh27600@gmail.com>
2023-02-02 10:55:08 +0700

    add prisma

--- pages/api/shorten.ts
+++ pages/api/shorten.ts
@@ -1,8 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import {PrismaClient} from "@prisma/client";
 import type {NextApiRequest, NextApiResponse} from "next";
-
 import {createClient} from "redis";
 import {
+	LIMIT_URL_HOUR,
 	LIMIT_URL_NUMBER,
 	LIMIT_URL_SECOND,
 	NUM_CHARACTER_HASH,
@@ -11,6 +12,8 @@ import HttpStatusCode from "utils/statusCode";
 import {generateRandomString, isValidUrl} from "utils/text";
 
 const client = createClient();
+
+const prisma = new PrismaClient();
 
 client.on("error", (err) => console.log("Redis Client Error", err));
 
@@ -25,40 +28,70 @@ export default async function handler(
 	req: NextApiRequest,
 	res: NextApiResponse<Data>
 ) {
-	const ip = req.socket.remoteAddress;
-	const url = req.query.url as string;
-	if (!url) {
-		return res.status(HttpStatusCode.BAD_REQUEST).send({
-			errorMessage: "You have submitted wrong data, please try again",
-			errorCode: "BAD_REQUEST",
-		});
+	try {
+		const ip = req.socket.remoteAddress;
+		const url = req.query.url as string;
+		if (!url || !ip) {
+			return res.status(HttpStatusCode.BAD_REQUEST).send({
+				errorMessage: "You have submitted wrong data, please try again",
+				errorCode: "BAD_REQUEST",
+			});
+		}
+		if (!isValidUrl(url)) {
+			return res.status(HttpStatusCode.BAD_REQUEST).send({
+				errorMessage: "Wrong URL format, please try again",
+				errorCode: "INVALID_URL",
+			});
+		}
+		// CHECK CURRENT LIMIT OR RESET LIMIT
+		await client.connect();
+		const keyLimit = `limit:${ip}`;
+		const ttl = await client.ttl(keyLimit);
+		if (ttl < 0) {
+			await client.set(keyLimit, 0);
+			await client.expire(keyLimit, LIMIT_URL_SECOND);
+		}
+		const curLimit = (await client.get(keyLimit)) || "";
+		const targetHash = generateRandomString(NUM_CHARACTER_HASH);
+		if (parseFloat(curLimit) >= LIMIT_URL_NUMBER) {
+			res.status(HttpStatusCode.UNAUTHORIZED).send({
+				errorMessage: `Exceeded ${LIMIT_URL_NUMBER} shorten links, please comeback after ${LIMIT_URL_HOUR} hours.`,
+				errorCode: "UNAUTHORIZED",
+			});
+		} else {
+			// INCR LIMIT, UPDATE LAST HISTORY IN REDIS AND NEW HISTORY IN DB
+			await client.incr(keyLimit);
+			const keyHash = `history:${ip}`;
+			let clientRedisId = await client.hGet(keyHash, "dbId");
+			if (!clientRedisId) {
+				const record = await prisma.urlShortenerRecord.create({
+					data: {ip},
+				});
+				clientRedisId = record.id + "";
+			}
+			const dataHash = [
+				"lastUrl",
+				url,
+				"lastHash",
+				targetHash,
+				"dbId",
+				clientRedisId,
+			];
+			await client.hSet(keyHash, dataHash);
+			const history = await prisma.urlShortenerHistory.create({
+				data: {
+					url,
+					hash: targetHash,
+					urlShortenerRecordId: +clientRedisId!,
+				},
+			});
+			res.status(HttpStatusCode.OK).json({url, hash: targetHash});
+		}
+		await client.disconnect();
+		await prisma.$disconnect();
+	} catch (error) {
+		console.error(error);
+		await client.disconnect();
+		await prisma.$disconnect();
 	}
-	if (!isValidUrl(url)) {
-		return res.status(HttpStatusCode.BAD_REQUEST).send({
-			errorMessage: "Wrong URL format, please try again",
-			errorCode: "INVALID_URL",
-		});
-	}
-	await client.connect();
-	const keyLimit = `limit:${ip}`;
-	const ttl = await client.ttl(keyLimit);
-	if (ttl < 0) {
-		await client.set(keyLimit, 0);
-		await client.expire(keyLimit, LIMIT_URL_SECOND);
-	}
-	const value = (await client.get(keyLimit)) || "";
-	const randomHash = generateRandomString(NUM_CHARACTER_HASH);
-	if (parseFloat(value) >= LIMIT_URL_NUMBER) {
-		res.status(HttpStatusCode.UNAUTHORIZED).send({
-			errorMessage:
-				"Exceeded 3 shorten links, please comeback after 24 hours.",
-			errorCode: "UNAUTHORIZED",
-		});
-	} else {
-		await client.incr(keyLimit);
-		const keyHash = `hash:${ip}`;
-		await client.hSet(keyHash, ["lastUrl", url, "lastHash", randomHash]);
-		res.status(HttpStatusCode.OK).json({url, hash: randomHash});
-	}
-	await client.disconnect();
 }
--- types/constants.ts
+++ types/constants.ts
@@ -1,4 +1,5 @@
-export const LIMIT_URL_SECOND = 24 * 3600;
+export const LIMIT_URL_HOUR = 1;
+export const LIMIT_URL_SECOND = LIMIT_URL_HOUR * 3600;
 export const LIMIT_URL_NUMBER = 3;
 export const NUM_CHARACTER_HASH = 5;
 export const BASE_URL = () => {
