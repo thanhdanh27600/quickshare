bcaf8a8d6a4500d7e58e987e5497489b6a9f91a6
thanhdanh27600 <thanhdanh27600@gmail.com>
2023-02-11 23:35:47 +0700

    test cicd

--- decrypt_secret.sh
+++ decrypt_secret.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-# Decrypt the file
-mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
---output $HOME/secrets/prod.env prod.env.gpg
+--output $HOME/prod.env prod.env.gpg
