0f997ab6dfe7cc4edcd7b2436738bcf131be37c7
thanhdanh27600 <thanhdanh27600@gmail.com>
2023-02-12 12:48:48 +0700

    fix dark

--- components/atoms/Input.tsx
+++ components/atoms/Input.tsx
@@ -18,7 +18,7 @@ export const Input = forwardRef(
 			<input
 				{...props}
 				ref={ref}
-				className="block h-12 w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-xl text-gray-900 focus:border-cyan-500 focus:ring-cyan-500 focus-visible:outline-cyan-500 dark:border-cyan-600 dark:bg-cyan-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-cyan-500 dark:focus:ring-cyan-500"
+				className="block h-12 w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-xl text-gray-900 focus:border-cyan-500 focus:ring-cyan-500 focus-visible:outline-cyan-500"
 			/>
 		);
 	}
