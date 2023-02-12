1241554fc952e49353949cb81db2e4c7b7a79341
thanhdanh27600 <thanhdanh27600@gmail.com>
2023-02-12 11:48:10 +0700

    fix env

--- types/constants.ts
+++ types/constants.ts
@@ -4,7 +4,7 @@ export const LIMIT_URL_NUMBER = 5;
 export const NUM_CHARACTER_HASH = 5;
 export const brandUrl = "https://clickdi.top";
 export const BASE_URL = () => {
-	const isProduction = process.env.NODE_ENV === "development";
+	const isProduction = process.env.NODE_ENV === "production";
 	if (isProduction) return brandUrl;
 	return "http://localhost:5000";
 };
