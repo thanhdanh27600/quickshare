001c6fdf889a88cabea51a78c7ed0a5885050be9
thanhdanh27600 <thanhdanh27600@gmail.com>
2023-02-19 15:43:54 +0700

    fix url

--- types/constants.ts
+++ types/constants.ts
@@ -4,10 +4,10 @@ export const LIMIT_URL_NUMBER = 5;
 export const NUM_CHARACTER_HASH = 5;
 export const brandUrl = 'https://clickdi.top';
 export const baseUrl = () => {
-  return 'https://114d-58-187-186-116.ap.ngrok.io/';
   const isProduction = process.env.NODE_ENV === 'production';
   if (isProduction) return brandUrl;
   return 'http://localhost:5000';
+  return 'https://114d-58-187-186-116.ap.ngrok.io/';
 };
 export const BASE_URL = baseUrl();
 export const REDIS_KEY = {
