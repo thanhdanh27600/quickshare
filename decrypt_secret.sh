e8ebf082f8cf4b5ec7f6a894c22df4ece1030c65
thanhdanh27600 <thanhdanh27600@gmail.com>
2023-03-12 14:19:40 +0700

    fix env

--- decrypt_secret.sh
+++ decrypt_secret.sh
@@ -3,4 +3,5 @@
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
---output ./.env .env.production.gpg
+--output ./.env.production .env.production.gpg
+mv ./.env.production ./.env
