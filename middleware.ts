4a5c6f3f9a55f94efe38b7160ae4d1387491a617
thanhdanh27600 <thanhdanh27600@gmail.com>
2023-06-26 10:23:40 +0700

    hot fix redis config

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 // This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
   return NextResponse.next();
 }
 
