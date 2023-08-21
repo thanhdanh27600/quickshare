7cb7e4c08f3ae7a8c5ead98ff0e92d4f6b4c82f6
thanhdanh27600 <thanhdanh27600@gmail.com>
2023-08-21 18:40:51 +0700

    remove clg

--- components/screens/RedirectShortDomain.tsx
+++ components/screens/RedirectShortDomain.tsx
@@ -2,12 +2,9 @@ import { useEffect } from 'react';
 import { BASE_URL, BASE_URL_SHORT } from 'types/constants';
 
 export const RedirectShortDomain = () => {
-  console.log('RedirectShortDomain run');
   useEffect(() => {
     const query = new URLSearchParams(window.location.search);
-    console.log('query', query);
     const hash = query.get('hash');
-    console.log('hash', hash);
     if (hash) {
       window.location.href = `${BASE_URL_SHORT}/${hash}`;
     } else {
