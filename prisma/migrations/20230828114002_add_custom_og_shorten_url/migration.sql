5ce0471d2487ef582ce334ef7b964d7266e3176a
thanhdanh27600 <thanhdanh27600@gmail.com>
2023-08-28 18:44:48 +0700

    add migration

---
+++ prisma/migrations/20230828114002_add_custom_og_shorten_url/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "UrlShortenerHistory" ADD COLUMN     "ogDescription" TEXT,
+ADD COLUMN     "ogDomain" TEXT,
+ADD COLUMN     "ogImgSrc" TEXT,
+ADD COLUMN     "ogTitle" TEXT;
