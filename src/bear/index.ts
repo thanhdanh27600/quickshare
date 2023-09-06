b6c1477eb608fb87bfd38f67ecd69d13fc106daa
thanhdanh27600 <thanhdanh27600@gmail.com>
2023-09-06 19:25:10 +0700

    fix local bear store

--- src/bear/index.ts
+++ src/bear/index.ts
@@ -1,19 +1,18 @@
 import { Window, isProduction } from 'types/constants';
-import { UseBoundStore } from 'zustand';
-import { devtools } from 'zustand/middleware';
 import exampleSlice from './exampleSlice';
 import shortenSlice from './shortenSlice';
 import utilitySlice from './utilitySlice';
 
-export const withDevTools = (slice: UseBoundStore<any>) => {
-  Window()[slice] = slice;
-  return devtools(slice, { enabled: !isProduction });
-};
-
 export const useBearStore = () => {
-  return {
+  const store = {
     exampleSlice,
     utilitySlice,
     shortenSlice,
   };
+
+  if (!isProduction && Window()) {
+    Window().bear = store;
+  }
+
+  return store;
 };
